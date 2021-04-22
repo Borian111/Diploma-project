af6a7fb374cd26d49f62a8b0ed16c1a412d885a0
Boris Tsarfin <legirovanniy@mail.ru>
2021-04-23 03:19:50 +0400

    2

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 //Modal window
+const modalOverlay = document.querySelector('.modal-overlay'),
+callbackBtn = document.querySelectorAll('.callback-btn'),
+
+modalClose = document.querySelector('.modal-close'),
+modalCallback = document.querySelector('.modal-callback'),
+buttonServices = document.querySelector('.button-services'),
+aaa = document.querySelectorAll('.row');
+console.log(callbackBtn)
 const modalWindow = () => { 
-    const modalOverlay = document.querySelector('.modal-overlay'),
+   /* const modalOverlay = document.querySelector('.modal-overlay'),
           callbackBtn = document.querySelectorAll('.callback-btn'),
           modalClose = document.querySelector('.modal-close'),
-          modalCallback = document.querySelector('.modal-callback');
+          modalCallback = document.querySelector('.modal-callback');*/
     let count = -48; 
 
 
@@ -17,18 +25,28 @@ const modalWindow = () => {
         }
     }
 
-    callbackBtn.forEach((elem) => { 
-        elem.addEventListener('click', () => {
+    callbackBtn[1].addEventListener('click', () => {
             modalOverlay.style.display = 'block';
             modalCallback.style.display = 'block';
             goGo();
         });
-    });
+    
 
     modalClose.addEventListener('click', () => { 
         modalOverlay.style.display = 'none';
         modalCallback.style.display = 'none';
    });
+
+   buttonServices.addEventListener('click', ()=>{
+       modalOverlay.style.display = 'block';
+       modalCallback.style.display = 'block';
+       goGo();
+   })
+
+   /*aaa.addEventListener('click', ()=>{
+    modalOverlay.style.display = 'block';
+    modalCallback.style.display = 'block';
+})*/
 
    modalOverlay.addEventListener('click', (event) =>{
          let target = event.target;
@@ -37,8 +55,18 @@ const modalWindow = () => {
                 modalOverlay.style.display = 'none';
                 modalCallback.style.display = 'none';
              }
+             if(target.classList.contains('modal-close')){
+                modalOverlay.display = 'none';
+                modalCallback.style.display = 'none';
+             }
     });
-};
+
+}
 
 modalWindow();
+
+//qqq
+
+
+
 
