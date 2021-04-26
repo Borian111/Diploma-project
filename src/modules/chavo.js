424433b1e940635c0f5f0b6b8d75daa45d4a368d
Boris Tsarfin <legirovanniy@mail.ru>
2021-04-27 00:24:17 +0400

    final1

--- src/modules/chavo.js
+++ src/modules/chavo.js
@@ -1,30 +1,30 @@
 const chavo = () => {
     
-    const element = document.querySelectorAll('.element')
+    const element = document.querySelectorAll('.element');
     
     for (let i=16; i<20; i++){
-        element[i].id='jjj'
+        element[i].id='jjj';
     }
     
-    const elementJJJ = document.querySelectorAll('#jjj')
+    const elementJJJ = document.querySelectorAll('#jjj');
         
     elementJJJ.forEach(item => {
-        const elCont = item.querySelector('.element-content')
+        const elCont = item.querySelector('.element-content');
         item.addEventListener('click', () => {                
                    
-          if (item.classList.contains("active")) {
-                item.classList.remove('active')
-                elCont.style.display = 'none'             
-           }else{
-                item.classList.add('active')
-                    elementJJJ.forEach(item => {
-                        item.querySelector('.element-content').style.display = 'none'
-                    })
-                elCont.style.display = 'block'
+          if  (item.classList.contains("active")) {
+                  item.classList.remove('active');
+                  elCont.style.display = 'none' ;            
+          }else{              
+              elementJJJ.forEach(item => {
+                  item.classList.remove('active');  
+                  item.querySelector('.element-content').style.display = 'none';
+                  });
+              item.classList.add('active');
+              elCont.style.display = 'block';
            }        
-        
-        })
-    })
-}
+        });
+    });
+};
 
 export default chavo;
--- src/modules/validatorX.js
+++ src/modules/validatorX.js
@@ -1,7 +1,7 @@
 const validatorX = () =>{
     const name = () => {
-        const name = document.querySelectorAll('input[name=fio]'); 
-            name.forEach((elem) =>{
+        const nam = document.querySelectorAll('input[name=fio]'); 
+            nam.forEach((elem) =>{
                 elem.addEventListener('input', (elem)=>{
                     elem.target.value = elem.target.value.replace(/[^а-яА-ЯёЁ\s+]/g, '')
                 })    
@@ -9,29 +9,43 @@ const validatorX = () =>{
                     elem.target.value = elem.target.value.replace(/\s+/g, ' ');
                     elem.target.value = elem.target.value.replace(/^\s+|\s{2,}|\s+$/g,'');
                    
-                    if(elem.target.value !==''){
+                    if(elem.target.value !=='' && elem.target.value.length>2){
                         elem.target.value = elem.target.value.toLowerCase();
                         elem.target.value = elem.target.value.split(/\s/).map(word => word[0].toUpperCase() + word.substring(1)).join(' ')
-                    }               
+                    }
+                    else{
+                      alert('В поле "Ваше имя" должно быть не менее 2 символов')
+                      elem.target.value =''
+                    }
                 })           
                               
+            })           
+        }       
+    
+    name();    
+        
+        const allPhone = () => {
+          
+            const phone =  document.querySelectorAll('input[name=tel]');         
+              
+             phone.forEach((elem) =>{
+                  
+                  elem.addEventListener('input', (elem) => {
+                    elem.target.value = elem.target.value.replace(/[^0-9\+]/g, '');
+                    elem.target.value = elem.target.value
+                    })  
+                    
+                  elem.addEventListener('blur', (elem) => {            
+                    console.log(elem.target.value)    ;              
+                      if(elem.target.value.length<7 || elem.target.value.length>11){
+                          alert('В поле "Телефон" должно быть от 7 до 11 символов');
+                          elem.target.value ='';
+                      }                 
+                  })    
             })
         }
         
-    name();    
-        
-    const allPhone = () => {
-          
-        const phone = document.querySelectorAll('input[name=tel]');         
-                
-        phone.forEach((elem) =>{
-            elem.addEventListener('input', (elem) => {
-            elem.target.value = elem.target.value.replace(/[^0-9\+]/g, '')
-            })          
-        })
-    }
-        
         allPhone();
-}
+    }
 
 export default validatorX;
